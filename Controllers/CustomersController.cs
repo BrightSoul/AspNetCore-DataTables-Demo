699fabb2f986ba0fa571bce3a88db805075eb7ca
Moreno Gentili <moreno@smartmobile.it>
2019-09-10 01:20:52 +0200

    Aggiunto attributo HttpPost su CustomersController

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -15,6 +15,7 @@ namespace DataTables.Controllers
 
         //Quest'action fornisce i dati a DataTables e deve chiamarsi come l'action a cui si riferisce + il suffisso Data
         //Dato che si riferisce alla DataTables presentata in Index, ecco che il suo nome deve essere IndexData
+        [HttpPost]
         public async Task<IActionResult> IndexData(DataTablesInputModel inputModel, [FromServices] DataTablesService dataTablesService)
         {
             var result = await dataTablesService.GetResultsAsync(inputModel);
